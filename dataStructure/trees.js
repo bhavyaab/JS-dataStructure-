4138919b4b7e62c473faacf437fad4585bd4633a
Bhavya Bharti <bhavyab@outlook.com>
2017-01-30 15:52:45 -0800

    refactor tree

--- dataStructure/trees.js
+++ dataStructure/trees.js
@@ -2,41 +2,29 @@
 
 const Queue = require('./queue.js');
 
-module.exports = Tree2;
+module.exports = Tree;
 
-function Tree1(val){
- this.val = val;
- this.children = []
- }
- var root = new Tree1('A');
- root.children.push(new Tree1('B'), new Tree1('C'));
- root.children[0].children.push(new Tree1('D'));
-
-
-//////////////////////////////////////////////
- function Tree2(val, kids){
+function Tree(val){
   this.val = val;
-  this.children = kids
-  }
+  this.children = [];
+}
 
-  //visit all children at the same level at the same time:
-
-  Tree2.prototype.print = function () {
-   let q = new Queue();
-   q.enqueue(this);
-    while(q.head){
-     var current = q.dequeue();
-     console.log(current.val);
-     current.children.forEach(function(ele){
+Tree.prototype.print = function () {
+  let q = new Queue();
+  q.enqueue(this);
+  while(q.head){
+    var current = q.dequeue();
+    console.log(current.val);
+    current.children.forEach(function(ele){
       q.enqueue(ele);
     });
-   }
-  };
+  }
+};
 
-var newRoot = new Tree2('A', [new Tree2('B', [new Tree2('D', [])]), new Tree2('C', [])]);
 
-// root.print();
+var root = new Tree('A');
+root.children.push(new Tree('B'), new Tree('C'));
+root.children[0].children.push(new Tree('D'));
 
-console.log('newRoot\n', root);
-
-newRoot.print();
+console.log('Tree = \n', root);
+root.print();
