b7a27f578e5328f455d5c9508667fb95ab4e7563
Bhavya Bharti <bhavyab@outlook.com>
2017-03-31 14:59:34 -0700

    command line inpt using process.argv

--- dataStructure/trangleArea.js
+++ dataStructure/trangleArea.js
@@ -1,23 +1,23 @@
+
 // Function Name : cal_area
 // This function has 1 parameter
 // triangle aTriangle:  It contains the 3 vertices of the triangle in x-y plane.
 // The function should return the area of the triangle "aTriangle".
 
 
-// commandline input:
-
-process.stdin.setEncoding('utf8');
-
-console.log('Enter (x, y) points of triangle!');
-
-process.stdin.on('data', function (text) {
-  console.log('received data:', text);
-  if (text === 'quit\n') {
-    done();
-  }
+// commandline input using readline:
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-function done() {
-  console.log('Now that process.stdin is paused, there is nothing more to do.');
-  process.exit();
-}
+// all points of a trangle.
+var trangle = process.argv.map(function(ele){
+  return parseInt(ele);
+}).slice(2);
+
+var cal_area = Math.round(Math.sqrt(Math.pow(2, trangle[0] - trangle[2]) + Math.pow(2, trangle[1] - trangle[3]) + Math.pow(2, trangle[3] - trangle[5]) + Math.pow(2, trangle[2] - trangle[4]) + Math.pow(2, trangle[5] - trangle[1]) + Math.pow(2, trangle[4] - trangle[0])));
+
+console.log('area of the trangle = ', cal_area);
+process.exit;
