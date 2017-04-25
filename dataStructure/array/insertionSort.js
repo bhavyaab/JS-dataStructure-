2e3109e01272731372f9edf7b3e3fbe40e7a5c9d
Bhavya <bhavyab@outlook.com>
2017-04-25 16:05:20 -0700

    palindrome

--- dataStructure/array/insertionSort.js
+++ dataStructure/array/insertionSort.js
@@ -1,24 +1,32 @@
 'use strict';
 
-var insert = function(array, rightIndex, value) {
-  var i;
+var insert = function(array, rightIndex, value)
+  var i = 0;
   for(i = rightIndex; i >= 0 && value < array[i]; i--){
     array[i + 1] = array[i];
   }
   array[i + 1] = value;
 };
-
+var insertionSort = function(array) {
+  for(var i = 1; i < array.length; i++) {
+    insert(array, i - 1, array[i]);
+  }
+  console.log('Insertion sorted array = ', array);
+};
+//Worst case: Θ(n​xn).
+// Best case: Θ(n).
+// Average case for a random array:  Θ(n​xn).
 
 var array = [3, 5, 7, 11, 13, 2, 9, 6];
-
-insert(array, 4, 2);
-console.log('Array after inserting 2:  ' + array);
+insertionSort(array);
+// insert(array, 4, 2);
+// console.log('Array after inserting 2:  ' + array);
 //Program.assertEqual(array, [2, 3, 5, 7, 11, 13, 9, 6]);
 
-insert(array, 5, 9);
-console.log('Array after inserting 9:  ' + array);
+// insert(array, 5, 9);
+// console.log('Array after inserting 9:  ' + array);
 //Program.assertEqual(array, [2, 3, 5, 7, 9, 11, 13, 6]);
 
-insert(array, 6, 6);
-console.log('Array after inserting 6:  ' + array);
+// insert(array, 6, 6);
+// console.log('Array after inserting 6:  ' + array);
 //Program.assertEqual(array, [2, 3, 5, 6, 7, 9, 11, 13]);
