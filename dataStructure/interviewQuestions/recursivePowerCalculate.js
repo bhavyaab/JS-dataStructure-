834ca7001568b8fcbdf033ddfed7c7196366487d
Bhavya <bhavyab@outlook.com>
2017-04-25 17:10:12 -0700

    recursice power calc

---
+++ dataStructure/interviewQuestions/recursivePowerCalculate.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var isEven = function(n) {
+  return n % 2 === 0;
+};
+
+var isOdd = function(n) {
+  return !isEven(n);
+};
+
+var power = function(x, n) {
+  console.log('Computing ' + x + ' raised to power ' + n + '.');
+    // base case
+  if(n === 0){
+    return 1;
+  }
+    // recursive case: n is negative
+  if(n < 0 ){
+    return 1 / power(x,-n);
+  }
+  if(n > 0 ){
+// recursive case: n is odd
+    if(isOdd(n)){
+      return x * power(x,n - 1);
+    }
+// recursive case: n is even
+    if(isEven(n)){
+      var y = power(x,n/2);
+      return y * y; // it reduces the complaxity order
+    }
+  }
+};
+
+var displayPower = function(x, n) {
+  console.log(x + ' to the ' + n + ' is ' + power(x, n));
+};
+
+displayPower(3, 0);
+displayPower(3, 1);
+displayPower(3, 4);
+displayPower(3, -2);
